5abfe169fa5b8727137807191c50ea3d3ab4a3c7
Jordi <ledfusion@users.noreply.github.com>
2019-02-11 19:14:21 +0100

    Splitting demo code into NodeJS and Browser

---
+++ demo/demo-browser.js
@@ -0,0 +1,114 @@
+const { FPVoter, FPCensus } = require("../src/franchiseproof.js")
+const zkSnark = require("snarkjs")
+const { bigInt } = zkSnark
+
+///////////////////////////////////////////////////////////////////////////////
+// BIG INT HELPERS
+
+function unstringifyBigInts(o) {
+    if ((typeof (o) == "string") && (/^[0-9]+$/.test(o))) {
+        return bigInt(o)
+    } else if (Array.isArray(o)) {
+        return o.map(unstringifyBigInts)
+    } else if (typeof o == "object") {
+        const res = {}
+        for (let k in o) {
+            res[k] = unstringifyBigInts(o[k])
+        }
+        return res
+    } else {
+        return o
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+async function generateWitness(circuit) {
+    // ORGANIZER SIDE
+    console.log("😺 generateWitness() ...")
+    console.time("😺 generateWitness()")
+    const voter = new FPVoter(
+        1337,
+        "0001020304050607080900010203040506070809000102030405060708090021"
+    )
+    const census = new FPCensus(20)
+    await census.add(voter.idx, await voter.getPublicKeyHash())
+
+    // VOTER SIDE
+    const poi = await census.proofOfInclusion(voter.idx)
+    const votingId = bigInt(1)
+    const voteValue = bigInt(2)
+    const input = await voter.getInput(votingId, voteValue, poi)
+
+    const witness = circuit.calculateWitness(input)
+    console.timeEnd("😺 generateWitness()")
+    return witness
+}
+
+function checkWitness(circuit, witness) {
+    console.log("😺 checkWitness ...")
+    console.time("😺 checkWitness")
+    if (!circuit.checkWitness(witness)) {
+        throw new Error("Cannot verify witness")
+    }
+    console.timeEnd("😺 checkWitness")
+}
+
+function createProof(provingKey, witness) {
+    // create proof ---------------------------------------------------------
+    console.log("😺 createProof() ...")
+    console.time("😺 createProof()")
+    // const provingKey = unstringifyBigInts(JSON.parse(fs.readFileSync("proving_key.json", "utf8")))
+    const { protocol } = provingKey
+    const { proof, publicSignals } = zkSnark[protocol].genProof(provingKey, witness)
+    // fs.writeFileSync("proof.json", JSON.stringify(stringifyBigInts(proof), null, 1), "utf-8")
+    // fs.writeFileSync("public_signals.json", JSON.stringify(stringifyBigInts(publicSignals), null, 1), "utf-8")
+    console.timeEnd("😺 createProof()")
+
+    return {
+        proof,
+        publicSignals
+    }
+}
+
+function isProofValid(verificationKey, proof, publicSignals) {
+    console.log("😺 isProofValid() ...")
+    console.time("😺 isProofValid()")
+    const verificationKeyProtocol = verificationKey.protocol
+    const isValid = zkSnark[verificationKeyProtocol].isValid(verificationKey, proof, publicSignals)
+    console.timeEnd("😺 isProofValid()")
+
+    return isValid
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+async function main() {
+    console.log("This is going to take some time 😁 ")
+
+    // COMPILE CIRCUIT
+    let circuitSource = unstringifyBigInts(require("./circuit.json"))
+
+    const circuit = new zkSnark.Circuit(circuitSource);
+
+    // GENERATE SETUP
+    let provingKey, verificationKey
+    provingKey = unstringifyBigInts(require("./proving_key.json"))
+    verificationKey = unstringifyBigInts(require("./verification_key.json"))
+
+    // GENERATE WITNESS
+    const witness = await generateWitness(circuit)
+
+    // CHECK IT
+    checkWitness(circuit, witness)
+
+    // CREATE PROOF
+    const { proof, publicSignals } = createProof(provingKey, witness)
+
+    const validProof = isProofValid(verificationKey, proof, publicSignals)
+
+    console.log("IS VALID:", validProof)
+}
+
+main()
+
