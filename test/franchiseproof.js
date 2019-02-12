697af1ba4bf8012ab1eaf73a84659ba1aa15ca0c
Jordi <ledfusion@users.noreply.github.com>
2019-02-12 20:44:09 +0100

    Updating the specs

--- test/franchiseproof.js
+++ test/franchiseproof.js
@@ -5,27 +5,26 @@ const compiler = require("circom");
 const bigInt = require("snarkjs").bigInt;
 const assert = chai.assert;
 
-const fpcensus = require("../src/franchiseproof").fpcensus;
-const fpvoter = require("../src/franchiseproof").fpvoter;
+const { FPCensus, FPVoter } = require("../src/franchiseproof");
 
 describe("FranchiseProof", function () {
     this.timeout(200000);
 
     it("Test franchise proof", async () => {
-        const voter = new fpvoter(
+        const voter = new FPVoter(
             1337,
             "0001020304050607080900010203040506070809000102030405060708090021"
         );
-        const census = new fpcensus(10);
-        await census.add(voter.idx,await voter.getPublicKeyHash());
-        
-        const poi        = await census.proofOfInclusion(voter.idx);
-        const votingId   = bigInt(1);
-        const voteValue  = bigInt(2);
-        const input      = await voter.getInput(votingId,voteValue,poi);
+        const census = new FPCensus(10);
+        await census.add(voter.idx, await voter.getPublicKeyHash());
+
+        const poi = await census.proofOfInclusion(voter.idx);
+        const votingId = bigInt(1);
+        const voteValue = bigInt(2);
+        const input = await voter.getInput(votingId, voteValue, poi);
 
         const cirDef = await compiler(path.join(__dirname, "circuits", "testfranchiseproof.circom"));
-        circuit      = new snarkjs.Circuit(cirDef);
+        circuit = new snarkjs.Circuit(cirDef);
         const w = circuit.calculateWitness(input);
         assert(circuit.checkWitness(w));
     })
